0ddc8ffe7dec7685168b7ea4a205175a3fceef10
bmpschneider <brunopschneider@gmail.com>
2020-04-10 18:44:49 -0300

    finalizado

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:fluttergifconverter/ui/gif_page.dart';
+// importando a home page da pasta ui
+import 'package:fluttergifconverter/ui/home_page.dart';
+import 'package:fluttergifconverter/ui/gif_page.dart';
+
+void main () {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
